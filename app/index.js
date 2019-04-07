073984de18202f6533b0b45251c9f3ebb1f8dd5a
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2019-04-06 17:38:18 -0700

    Log heart rate

--- app/index.js
+++ app/index.js
@@ -1,1 +1,22 @@
-console.log('Hello world!');
+/*
+  Modules
+*/
+import { HeartRateSensor } from 'heart-rate';
+const SECOND = 1000;
+
+/*
+  Setup
+*/
+const hrm = new HeartRateSensor();
+hrm.start();
+
+/*
+  Main
+*/
+function main() {
+  console.log('heart rate:', hrm.heartRate);
+}
+
+// Initialize the app. Will run the main function every second.
+main();
+setInterval(main, 1 * SECOND);
